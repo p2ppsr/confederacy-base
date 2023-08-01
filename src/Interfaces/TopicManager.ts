c0914f401c016900c71b76f2f35acfb68ce73687
Gabriel McCormick <74574750+ctrle-mg-resu@users.noreply.github.com>
2023-08-01 14:52:58 -0700

    Added get documentation method

--- src/Interfaces/TopicManager.ts
+++ src/Interfaces/TopicManager.ts
@@ -5,12 +5,13 @@ import { Output } from "../Models/Output";
  * Defines a Confederacy TopicManager interface that can be implemented
  */
 export interface TopicManager {
-    /**
-     * Returns the outputs from the transaction that are admissible.
-     * @param {Array<Output>} previousUTXOs - UTXOs belonging to the current topic being spent as input in the transaction
-     * @param {Transaction} parsedTransaction - transaction containing outputs to admit into the current topic
-     */
-    identifyAdmissibleOutputs({previousUTXOs, parsedTransaction}: {previousUTXOs: Array<Output>, parsedTransaction: Transaction}): Promise<number[] | AdmissableOutputs>
+  /**
+   * Returns the outputs from the transaction that are admissible.
+   * @param {Array<Output>} previousUTXOs - UTXOs belonging to the current topic being spent as input in the transaction
+   * @param {Transaction} parsedTransaction - transaction containing outputs to admit into the current topic
+   */
+  identifyAdmissibleOutputs({ previousUTXOs, parsedTransaction }: { previousUTXOs: Array<Output>, parsedTransaction: Transaction }): Promise<number[] | AdmissableOutputs>
+  getDocumentation?(): Promise<String>
 }
 
 /**
@@ -19,6 +20,6 @@ export interface TopicManager {
  * @param {number[]} outputsToRetain - references the ids of all outputs to retain as history
  */
 export type AdmissableOutputs = {
-    outputsToAdmit: number[]
-    outputsToRetain: number[]
+  outputsToAdmit: number[]
+  outputsToRetain: number[]
 }
